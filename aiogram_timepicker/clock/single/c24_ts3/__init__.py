05d670585ed6256f39ef86b6599d5f67364241eb
Marat Ashrafzianov <maratashdev@gmail.com>
2022-09-04 08:22:41 +0300

    clock with 8 numbers timestamp 3 (hours clock) added

---
+++ aiogram_timepicker/clock/single/c24_ts3/__init__.py
@@ -0,0 +1,17 @@
+from aiogram.utils.callback_data import CallbackData
+
+
+# setting timepicker_callback prefix and parts
+timepicker_callback = CallbackData('c24_3_tmpck', 'act', 'time')
+
+_default = {
+    'select': 'Select',
+    'cancel': 'Cancel',
+    'empty': ' ',
+    'center': '•',
+    'time_format': '{0:02}',
+    'time_current_format': '({0:02})',
+}
+
+from .timepicker import TimePicker
+from .utils import default
